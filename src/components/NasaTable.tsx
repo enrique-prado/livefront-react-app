3d9d23d68b3bfca56e707d1b018e8f485af78cf9
Enrique Prado <enrique.prado7@gmail.com>
2025-05-01 19:47:19 -0700

    Adding icons and hover logic
    formatting doc

--- src/components/NasaTable.tsx
+++ src/components/NasaTable.tsx
@@ -1,53 +1,69 @@
-import { useQuery } from '@tanstack/react-query'
-import { Link } from '@tanstack/react-router'
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
-const API_KEY = 'MetHYfQ8KJ7rodhMY82Kd320QG7mmer4L8Q5L0xC'
+const API_KEY = "MetHYfQ8KJ7rodhMY82Kd320QG7mmer4L8Q5L0xC";
 
 function NasaTable() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['data'],
-    queryFn: fetchImagesData,
-  })
+	const { isLoading, error, data } = useQuery({
+		queryKey: ["data"],
+		queryFn: fetchImagesData,
+	});
 
-  if (isLoading) return <p>Loading...</p>
-  if (error instanceof Error) return <p>Error: {error.message}</p>
+	if (isLoading) return <p>Loading...</p>;
+	if (error instanceof Error) return <p>Error: {error.message}</p>;
 
-  return (
-    <table className='table-nasa'>
-      <thead>
-        <tr>
-          <th className='px-4 py-2'>Date</th>
-          <th className='px-4 py-2'>Image Title</th>
-          <th className='px-4 py-2'>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item: { date: string; title: string; url: string }) => (
-          <tr className='hover-row' key={item.date}>
-            <td className='border px-4 py-2'>{item.date}</td>
-            <td className='border-x-0 px-4 py-2'>{item.title}</td>
-            <td className="border px-4">
+	return (
+		<table className="table-nasa">
+			<thead>
+				<tr>
+					<th className="px-4 py-2">Date</th>
+					<th className="px-4 py-2">Image Title</th>
+					<th className="px-4 py-2">Preview</th>
+					<th className="px-4 py-2">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data.map((item: { date: string; title: string; tumbnail_url: string }) => (
+					<tr className="hover-row group" key={item.date}>
+						<td className="border px-4 py-2">{item.date}</td>
+						<td className="border-x-0 px-4 py-2">{item.title}</td>
+						<td className="border-x-0 px-4 py-2">
+							<img
+								src={item.tumbnail_url}
+								alt={item.title}
+								className="thumb-image"
+							/>
+						</td>
+						<td className="border px-4">
 							<div className="relative">
 								<span className="invisible group-hover:visible">
-                  <Link to='/photoDetails/$date' params={{ date: item.date }} className="px-2 hover:bg-gray-200" >
-                    Details
-                  </Link>
+									<Link
+										to="/photoDetails/$date"
+										params={{ date: item.date }}
+										className="table-link"
+									>
+										<FontAwesomeIcon color="#3b82f6" icon={faEye} />
+									</Link>
 								</span>
 							</div>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
 
 async function fetchImagesData() {
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=30&thumbs=true`)
-  if (!response.ok) {
-    throw new Error('Network response was not ok')
-  }
-  return response.json()
+	const response = await fetch(
+		`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=30&thumbs=true`,
+	);
+	if (!response.ok) {
+		throw new Error("Network response was not ok");
+	}
+	return response.json();
 }
 
-export default NasaTable
+export default NasaTable;
